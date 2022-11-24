4be14b81b6627be5f8fe1496ce004b5bd8458d44
Jesus Angulo <jesus.angulo@outlook.com>
2022-11-23 19:02:48 -0500

    feat(books): register BookService

--- sample-api/Models/BookService.cs
+++ sample-api/Models/BookService.cs
@@ -1,7 +1,8 @@
 namespace sample_api.Models
 {
-    public class BookService{
-     private List<Book> _books = new List<Book>
+    public class BookService
+    {
+        private List<Book> _books = new List<Book>
         {
             new Book
             {
@@ -11,34 +12,34 @@ namespace sample_api.Models
             },
             new Book
             {
-                Id = 1,
-                Title = "Book 1",
-                Price = 59.99M
+                Id = 2,
+                Title = "Book 2",
+                Price = 14.99M
             },
             new Book
             {
-                Id = 1,
-                Title = "Book 1",
-                Price = 59.99M
+                Id = 3,
+                Title = "Book 3",
+                Price = 9.99M
             },
             new Book
             {
-                Id = 1,
-                Title = "Book 1",
-                Price = 59.99M
+                Id = 4,
+                Title = "Book 4",
+                Price = 19.99M
             }
         };
 
-    public IEnumerable<Book> GetAll()
-    {
-        return _books;
-    }
+        public IEnumerable<Book> GetAll()
+        {
+            return _books;
+        }
 
 
-    public Book GetById(int id)
-    {
-        return _books.FirstOrDefault(x => x.Id == id);
-    }
+        public Book GetById(int id)
+        {
+            return _books.FirstOrDefault(x => x.Id == id);
+        }
 
     }
 }
--- sample-api/Program.cs
+++ sample-api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<OrderService>();
+builder.Services.AddSingleton<BookService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
